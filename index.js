2f0a6b655118388e40f172061438302ee349336b
Sofia Katzman <katzmansof@gmail.com>
2023-02-25 18:43:07 -0500

    finished lab

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = function(){
+    return 2000/100
+};
+
+const square = digitToSquare => digitToSquare * digitToSquare
+
+const add = (digitToAddOne, digitToAddTwo) => digitToAddOne + digitToAddTwo
